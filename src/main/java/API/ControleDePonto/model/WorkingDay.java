ea4387a9d2e505d83b1fdec6ba02dcf972a2a457
uluizeduardo <luiz.analistadesistemas@gmail.com>
2021-10-12 22:29:26 -0300

    Created the workday class

---
+++ src/main/java/API/ControleDePonto/model/WorkingDay.java
@@ -0,0 +1,21 @@
+package API.ControleDePonto.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Builder
+public class WorkingDay {
+
+    private long id;
+    private String description;
+
+}
