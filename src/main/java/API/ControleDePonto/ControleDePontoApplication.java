8e96c331f1245aca7a36ccfd08123902276bca59
uluizeduardo <luiz.analistadesistemas@gmail.com>
2021-10-12 21:58:49 -0300

    Project created

---
+++ src/main/java/API/ControleDePonto/ControleDePontoApplication.java
@@ -0,0 +1,13 @@
+package API.ControleDePonto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleDePontoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleDePontoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/API/ControleDePonto/ControleDePontoApplicationTests.java
@@ -0,0 +1,13 @@
+package API.ControleDePonto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleDePontoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
