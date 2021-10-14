3781721ea322b75ae4d1bc74e086d68dc62f9ccc
uluizeduardo <luiz.analistadesistemas@gmail.com>
2021-10-13 22:12:38 -0300

    Created the user class

---
+++ src/main/java/API/ControleDePonto/model/User.java
@@ -0,0 +1,36 @@
+package API.ControleDePonto.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Builder
+public class User {
+
+    @Id
+    private Long id;
+    @ManyToOne
+    private UserCategory userCategory;
+    private String name;
+    @ManyToOne
+    private Company company;
+    @ManyToOne
+    private AccessLevel accessLevel;
+    @ManyToOne
+    private WorkingDay workingDay;
+    private BigDecimal tolerance;
+    private LocalDateTime journeyStart;
+    private LocalDateTime endJourney;
+}
