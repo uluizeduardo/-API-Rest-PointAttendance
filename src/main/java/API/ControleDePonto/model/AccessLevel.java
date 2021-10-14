7a139811c21cfbd6490dea31959353b80d88f18d
uluizeduardo <luiz.analistadesistemas@gmail.com>
2021-10-13 21:47:07 -0300

    Created the access level class

--- src/main/java/API/ControleDePonto/model/AccessLevel.java
+++ src/main/java/API/ControleDePonto/model/AccessLevel.java
@@ -13,14 +13,8 @@ import lombok.Setter;
 @NoArgsConstructor
 @EqualsAndHashCode
 @Builder
-public class Company {
+public class AccessLevel {
 
     private Long id;
     private String description;
-    private String cnpj;
-    private String address;
-    private String district;
-    private String city;
-    private String state;
-    private String telephone;
 }
