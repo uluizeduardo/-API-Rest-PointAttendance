c78a97a5a305475da212cc13f824275f0cca7c8c
uluizeduardo <luiz.analistadesistemas@gmail.com>
2021-10-13 21:39:01 -0300

    Created the company class

---
+++ src/main/java/API/ControleDePonto/model/Company.java
@@ -0,0 +1,8 @@
+
+
+package API.ControleDePonto.model;
+
+
+public class Company {
+
+}
--- src/main/java/API/ControleDePonto/model/UserCategory.java
+++ src/main/java/API/ControleDePonto/model/UserCategory.java
@@ -1,5 +1,18 @@
 package API.ControleDePonto.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Builder
 public class UserCategory {
 
     private long id;
