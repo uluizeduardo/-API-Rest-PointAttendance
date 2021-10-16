1ed4b15b45f36a4cdea1579581a0216c559aa53d
uluizeduardo <luiz.analistadesistemas@gmail.com>
2021-10-15 21:38:25 -0300

    Created the date type class

---
+++ src/main/java/API/ControleDePonto/model/DateType.java
@@ -0,0 +1,20 @@
+package API.ControleDePonto.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Builder
+public class DateType {
+
+    private Long id;
+    private String description;
+}
