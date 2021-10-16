66862c6e9626c059b75293bdd11fd6039e427ba7
uluizeduardo <luiz.analistadesistemas@gmail.com>
2021-10-15 21:34:52 -0300

    Created the occurrence class

---
+++ src/main/java/API/ControleDePonto/model/Occurrence.java
@@ -0,0 +1,22 @@
+package API.ControleDePonto.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Builder
+public class Occurrence {
+
+    private Long id;
+    private String name;
+    private String description;
+
+}
