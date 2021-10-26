8bca99ac552a293748c8ba559860935243c634dc
uluizeduardo <luiz.analistadesistemas@gmail.com>
2021-10-25 22:29:24 -0300

    adjusted end-point update

--- src/main/java/API/ControleDePonto/controller/WorkingDayController.java
+++ src/main/java/API/ControleDePonto/controller/WorkingDayController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,7 +34,7 @@ public class WorkingDayController {
         return ResponseEntity.ok(workingDayService.getById(idWorkingDay).orElseThrow(() -> new Exception("WorkingDay not found")));
     }
 
-    @PostMapping
+    @PutMapping
     public WorkingDay updateWorkingDay(@RequestBody WorkingDay workingDay) {
         return workingDayService.save(workingDay);
     }
